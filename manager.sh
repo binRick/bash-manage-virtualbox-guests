fb0d7af2f9da00768d7e60b3b920c3b221528e5c
richard <you@example.com>
2019-11-25 15:25:30 -0500

    m

--- manager.sh
+++ manager.sh
@@ -14,61 +14,6 @@ PUBIC_KEY_FILE=~/.ssh/id_rsa.pub
 [[ "$PASS" == "" ]] && export PASS=12341234
 COMMON_ARGS="--username root --password $PASS"
 
-success() {
-    local args=(
-        - "Creating VM"                                         create_vm    3.0
-        - "Creating ramdisk"                                    my_sleep     0.1
-        - "Enabling swap"                                       my_sleep     0.1
-        - "Populating VM with world data"                       restore_data 5.0
-        - "Spigot: Pulling docker image for build"              my_sleep     0.5
-        - "Spigot: Building JAR"                                my_sleep     6.0
-        - "Pulling remaining docker images"                     my_sleep     2.0
-        - "Launching services"                                  my_sleep     0.2
-
-        --require "Creating VM"
-        --before  "Creating ramdisk"
-        --before  "Enabling swap"
-
-        --require "Creating ramdisk"
-        --before  "Populating VM with world data"
-        --before  "Spigot: Pulling docker image for build"
-
-        --require "Spigot: Pulling docker image for build"
-        --before  "Spigot: Building JAR"
-        --before  "Pulling remaining docker images"
-
-        --require "Populating VM with world data"
-        --require "Spigot: Building JAR"
-        --require "Pulling remaining docker images"
-        --before  "Launching services"
-    )
-
-    concurrent "${args[@]}"
-}
-
-create_vm() {
-    local provider=digitalocean
-    echo "(on ${provider})" >&3
-    my_sleep "${@}"
-}
-
-restore_data() {
-    local data_source=dropbox
-    echo "(with ${data_source})" >&3
-    my_sleep "${@}"
-}
-
-my_sleep() {
-    local seconds=${1}
-    local code=${2:-0}
-    echo "Yay! Sleeping for ${seconds} second(s)!"
-    sleep "${seconds}"
-    if [ "${code}" -ne 0 ]; then
-        echo "Oh no! Terrible failure!" 1>&2
-    fi
-    return "${code}"
-}
-
 secureVM(){
 	VM="$1"
 	VBoxManage guestcontrol $VM -v $COMMON_ARGS \
@@ -185,18 +130,4 @@ createVM(){
 	showPortForwarding
 }
 
-main() {
-    if [[ -n "${1}" ]]; then
-        "${1}"
-    else
-        echo
-        echo "[SUCCESS EXAMPLE]"
-        success
-        echo
-        echo "[FAILURE EXAMPLE]"
-        failure
-    fi
-}
-
 createVM
-#main "${@}"
